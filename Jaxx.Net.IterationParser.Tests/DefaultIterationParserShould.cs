471d94b26229d0c0b7b816e5202a977ed5f220cd
viper3400 <viper_3400@gmx.de>
2019-01-15 18:21:52 +0100

    Fixed namespace typo

--- Jaxx.Net.IterationParser.Tests/DefaultIterationParserShould.cs
+++ Jaxx.Net.IterationParser.Tests/DefaultIterationParserShould.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using Xunit;
 
-namespace Jaxx.Net.IterationParser.tests
+namespace Jaxx.Net.IterationParser.Tests
 {
     public class DefaultIterationParserShould
     {
