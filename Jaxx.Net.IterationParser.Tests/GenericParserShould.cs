7fc925232bf9b4a088fd5b3a64ec2c785e1e5ae1
viper3400 <viper_3400@gmx.de>
2019-01-16 19:55:38 +0100

    updated unit tests

--- Jaxx.Net.IterationParser.Tests/GenericParserShould.cs
+++ Jaxx.Net.IterationParser.Tests/GenericParserShould.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using Xunit;
@@ -26,6 +27,138 @@ namespace Jaxx.Net.IterationParser.Tests
             Assert.Equal("COND", actual[1]["IterationResult"]);
             Assert.Equal("QA TL2", actual[1]["IterationType"]);
             Assert.Equal("QA TL2; 20.08.2018; COND", actual[1]["IterationLine"]);
+        }
+
+        [Fact]
+        public void SplitIterationLinesWithDefaultLineSelector()
+        {
+            var inputBuilder = new StringBuilder();
+            inputBuilder.AppendLine("QA TL1; 19.08.2018; PASSED");
+            inputBuilder.AppendLine("QA TL2; 20.08.2018; PASSED");
+            inputBuilder.Append("QA TL1; 21.08.2018; PASSED");
+            var input = inputBuilder.ToString();
+
+            var parser = new GenericParser();
+            var actual = parser.SplitIterationLines(input, new DefaultIterationRegExSelector().SingleLineSelector.Selector);
+            Assert.Equal(3, actual.Count());
+            Assert.Equal("QA TL1; 21.08.2018; PASSED", actual[2]);
+        }
+
+        [Fact]
+        public void SplitIterationLinesWithCustomerLimiter()
+        {
+            var input = "QA TL1; 19.08.2018; PASSED###QA TL2; 20.08.2018; PASSED###QA TL1; 21.08.2018; PASSED";
+            var parser = new GenericParser();
+            var actual = parser.SplitIterationLines(input, "###");
+            Assert.Equal(3, actual.Count());
+            Assert.Equal("QA TL1; 21.08.2018; PASSED", actual[2]);
+        }
+
+        [Fact]
+        public void ParseDateFromIterationLongFormat()
+        {
+            var input = "QA TL1; 19.08.2018; PASSED";
+
+            var parser = new GenericParser();
+            var actual = DateTime.ParseExact
+                (parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationDateSelector),
+                "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            Assert.Equal(DateTime.ParseExact("19.08.2018", "dd.MM.yyyy", CultureInfo.InvariantCulture), actual);
+        }
+
+        [Fact]
+        public void ParseDateFromIterationShortFormat()
+        {
+            var input = "QA TL1; 19.08.18; PASSED";
+
+            var parser = new GenericParser();
+            var actual = DateTime.ParseExact
+                (parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationDateSelector),
+                "dd.MM.yy", CultureInfo.InvariantCulture);
+
+            Assert.Equal(DateTime.ParseExact("19.08.2018", "dd.MM.yyyy", CultureInfo.InvariantCulture), actual);
+        }
+
+        [Fact]
+        public void ParseDateFromIterationLongFormatWithoutWhiteSpaces()
+        {
+            var input = "QA TL12;09.01.2018;FAILED";
+
+            var parser = new GenericParser();
+            var actual = DateTime.ParseExact
+                (parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationDateSelector),
+                "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            Assert.Equal(DateTime.ParseExact("09.01.2018", "dd.MM.yyyy", CultureInfo.InvariantCulture), actual);
+        }
+
+        [Fact]
+        public void ParseDateFromIterationCustomFormat()
+        {
+            var input = "QA TL1; 19.08.2018; PASSED";
+            var selector = new RegExSelector { Selector = @"(\d{2}.\d{2}.(20\d{2}|\d{2}))", SelectedMatchGroup = 0 };
+
+            var parser = new GenericParser();
+            var actual = DateTime.ParseExact
+                (parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationDateSelector),
+                "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            Assert.Equal(DateTime.ParseExact("19.08.2018", "dd.MM.yyyy", CultureInfo.InvariantCulture), actual);
+        }
+
+        [Fact]
+        public void ParseGenericString()
+        {
+            var input = "QA TL1; 19.08.2018; PASSED";
+            var selector = new RegExSelector { Selector = @"(\d{2}.\d{2}.(20\d{2}|\d{2}))", SelectedMatchGroup = 0 };
+
+            var parser = new GenericParser();
+            var actual = parser.GetGenericString(input, selector);
+
+            Assert.Equal("19.08.2018", actual);
+        }
+
+
+        [Fact]
+        public void ParseIterationTypeWithDefaultValue()
+        {
+            var input = "QA TL1; 19.08.2018; PASSED";
+
+            var parser = new GenericParser();
+            var actual = parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationTypeSelector);
+
+            Assert.Equal("QA TL1", actual);
+        }
+
+        [Fact]
+        public void ParseIterationCountWithDefaultValue()
+        {
+            var input = "QA TL6; 19.08.2018; PASSED";
+            var parser = new GenericParser();
+            var actual = int.Parse(parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationCountSelector));
+            Assert.Equal(6, actual);
+        }
+
+        [Fact]
+        public void ParseIterationCountWithDefaultValueAndTwoDecimals()
+        {
+            var input = "QA TL 16;19.08.2018; PASSED";
+            var parser = new GenericParser();
+            var actual = int.Parse(parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationCountSelector));
+            Assert.Equal(16, actual);
+        }
+
+
+        [Fact]
+        public void ParseIterationResultWithDefaultValue()
+        {
+            var input = "QA TL1; 19.08.2018;  PASSED  ";
+
+            var parser = new GenericParser();
+            var actual = parser.GetGenericString(input, new DefaultIterationRegExSelector().TestIterationResultSelector);
+
+            Assert.Equal("PASSED", actual);
         }
     }
 }
